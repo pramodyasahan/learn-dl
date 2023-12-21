a310b68ff0e06c27a1f2c4f6cc48b998dea59e3d
pramodyasahan <sahandil1013@gmail.com>
2023-12-22 02:22:26 +0530

    feat: add comments

--- ann/main.py
+++ ann/main.py
@@ -6,32 +6,57 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.metrics import confusion_matrix, accuracy_score
 
+# Load the dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
-X = dataset.iloc[:, 3:-1].values
-y = dataset.iloc[:, -1].values
 
+# Select features and target variable
+X = dataset.iloc[:, 3:-1].values  # Features (input variables)
+y = dataset.iloc[:, -1].values  # Target variable (output)
+
+# Encoding categorical data
 le = LabelEncoder()
-X[:, 2] = le.fit_transform(X[:, 2])
+X[:, 2] = le.fit_transform(X[:, 2])  # Encoding gender column
 
+# Applying OneHotEncoder to the 'Geography' column
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
-X = np.array(ct.fit_transform(X))
+X = np.array(ct.fit_transform(X))  # One-hot encoding for country
 
+# Splitting the dataset into the Training set and Test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
 
+# Feature Scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)  # Scaling training data
+X_test = sc.transform(X_test)  # Scaling test data
+
+# Initializing the ANN
 model = tf.keras.models.Sequential()
+
+# Adding the input layer and first hidden layer
 model.add(tf.keras.layers.Dense(units=6, activation='relu'))
+
+# Adding the second hidden layer
 model.add(tf.keras.layers.Dense(units=6, activation='relu'))
+
+# Adding the output layer
 model.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
 
+# Compiling the ANN
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Training the ANN on the Training set
 model.fit(X_train, y_train, epochs=100, batch_size=32)
 
+# Making predictions and evaluating the model
 y_pred = model.predict(X_test)
-y_pred = (y_pred > 0.5)
-print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+y_pred = (y_pred > 0.5)  # Converting probabilities to binary output
+
+# Comparing predicted results with actual results
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+# Making the Confusion Matrix
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
-accuracy_score(y_test, y_pred)
+
+# Calculating the accuracy
+print(accuracy_score(y_test, y_pred))
