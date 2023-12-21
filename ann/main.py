ccb1d279f41ae05797f7ab7aad3425235c6e7280
pramodyasahan <sahandil1013@gmail.com>
2023-12-22 02:10:09 +0530

    feat: import the dataset

--- ann/main.py
+++ ann/main.py
@@ -4,3 +4,9 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
+
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:-1].values
+y = dataset.iloc[:, -1].values
+
+
