aca553c4159a69fbc10e3e749908dea001162048
pramodyasahan <sahandil1013@gmail.com>
2023-12-22 03:47:21 +0530

    feat: import training set

--- cnn/main.py
+++ cnn/main.py
@@ -1,3 +1,6 @@
 import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
 
+train_datagen = ImageDataGenerator(rescale=1. / 255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+train_generator = train_datagen.flow_from_directory('C:\\Users\\user\\Desktop\\Workplace\\Machine Learning A-Z™ AI, Python & R\\dataset\\dataset\\training_set',
+                                                    target_size=(64, 64), batch_size=32, class_mode='binary')
