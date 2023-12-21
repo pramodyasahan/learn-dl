8e87b8f80064719ce99a697c0e060d2c839c39b1
pramodyasahan <sahandil1013@gmail.com>
2023-12-22 04:26:22 +0530

    feat: add comments

--- cnn/main.py
+++ cnn/main.py
@@ -3,33 +3,51 @@ import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
 from keras.preprocessing import image
 
+# Image Data Augmentation for training set
 train_datagen = ImageDataGenerator(rescale=1. / 255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
 train_generator = train_datagen.flow_from_directory(
-    'C:\\Users\\user\\Desktop\\Workplace\\Machine Learning A-Z™ AI, Python & R\\dataset\\dataset\\training_set',
+    'data/train',  # Updated path to training set
     target_size=(64, 64), batch_size=32, class_mode='binary')
 
+# Image Data Augmentation for test set
 test_datagen = ImageDataGenerator(rescale=1. / 255)
 validation_generator = test_datagen.flow_from_directory(
-    'C:\\Users\\user\\Desktop\\Workplace\\Machine Learning A-Z™ AI, Python & R\\dataset\\dataset\\test_set',
+    'data/test',  # Updated path to test set
     target_size=(64, 64), batch_size=32, class_mode='binary')
 
+# Building the CNN
 cnn = tf.keras.models.Sequential()
+
+# First Convolutional Layer
 cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu', input_shape=(64, 64, 3)))
 cnn.add(tf.keras.layers.MaxPooling2D(pool_size=2, strides=2))
+
+# Second Convolutional Layer
 cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'))
 cnn.add(tf.keras.layers.MaxPooling2D(pool_size=2, strides=2))
 
+# Flattening
 cnn.add(tf.keras.layers.Flatten())
+
+# Full Connection
 cnn.add(tf.keras.layers.Dense(units=128, activation='relu'))
+
+# Output Layer
 cnn.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
 
+# Compiling the CNN
 cnn.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Training the CNN on the Training set and evaluating it on the Test set
 cnn.fit_generator(train_generator, validation_data=validation_generator, epochs=25)
 
+# Making a single prediction
 test_image = image.load_img('cat.jpg', target_size=(64, 64))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis=0)
 result = cnn.predict(test_image)
+
+# Output result interpretation
 train_generator.class_indices
 if result[0][0] == 1:
     prediction = 'dog'
