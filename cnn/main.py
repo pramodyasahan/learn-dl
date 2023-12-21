ab49016222dde6f721f034a185ed5515fd13a2b0
pramodyasahan <sahandil1013@gmail.com>
2023-12-22 03:58:36 +0530

    feat: build the CNN model

--- cnn/main.py
+++ cnn/main.py
@@ -10,3 +10,9 @@ test_datagen = ImageDataGenerator(rescale=1. / 255)
 validation_generator = test_datagen.flow_from_directory(
     'C:\\Users\\user\\Desktop\\Workplace\\Machine Learning A-Z™ AI, Python & R\\dataset\\dataset\\test_set',
     target_size=(64, 64), batch_size=32, class_mode='binary')
+
+cnn = tf.keras.models.Sequential()
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu', input_shape=(64, 64, 3)))
+cnn.add(tf.keras.layers.MaxPooling2D(pool_size=2, strides=2))
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'))
+cnn.add(tf.keras.layers.MaxPooling2D(pool_size=2, strides=2))
